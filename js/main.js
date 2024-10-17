8cd85531cc9ff836074685a81e3fbdf622c448be
FrancescoGiommi <francescogiommi97@icloud.com>
2024-10-17 16:56:13 +0200

    Add new verify mail

--- js/main.js
+++ js/main.js
@@ -10,12 +10,20 @@ Chiedi all'utente la sua email,
 const userMail = "user@libero.it";
 
 /* lista mail che possono accedere */
-const mailListCanLog =
-  ("user@libero.it", "mail@tiscali.it", "username@gmail.it", "ciao@icloud.it");
+const mailListCanLog = [
+  "user@libero.it",
+  "mail@tiscali.it",
+  "username@gmail.it",
+  "ciao@icloud.it",
+];
 
 /* lista mail che non possono accedere */
-const mailListCantLog =
-  ("user@tiscali.it", "mail@libero.it", "username@icloud.it", "ciao@gmail.it");
+const mailListCantLog = [
+  "user@tiscali.it",
+  "mail@libero.it",
+  "username@icloud.it",
+  "ciao@gmail.it",
+];
 
 /* PROCEDURA */
 
@@ -24,29 +32,30 @@ const mailListCantLog =
 const emailRequest = prompt("Inserisci la tua email");
 
 /* controllo che la mail sia nella lista delle mail che possono accedere */
-/* se è nella lsita delle mail che possono accedere */
-if (emailRequest == userMail) {
-  alert("Questa mail può accedere");
-  console.log("Questa mail può accedere");
-  /* se non è nella lsita delle mail che possono accedere  */
-} else if (emailRequest !== userMail) {
-  alert("Questa mail non può accedere");
-  console.log("Questa mail non può accedere");
+for (let i = 0; i !== emailRequest; i++) {
+  if (emailRequest == mailListCanLog[i]) {
+    alert("Questa mail può accedere");
+    console.log("Questa mail può accedere");
+  } else if (emailRequest == mailListCantLog[i]) {
+    alert("Questa mail non può accedere");
+    console.log("Questa mail non può accedere");
+  }
 }
+/* se è nella lsita delle mail che possono accedere */
 
 /* OUTPUT */
-console.log(`
-    userMail: ${userMail}
-    mailListCanLog: ${mailListCanLog}
-    mailListCantLog: ${mailListCantLog}
-    emailRequest: ${emailRequest}
-    `);
 
 /*> Gioco dei dadi
 
 Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 > Stabilire il vincitore, in base a chi fa il punteggio più alto.*/
+/* RACCOLTA DATI */
 
+/* PROCEDURA */
+
+/* ELABORAZIONE */
+
+/* OUTPUT */
 /*Prima di partire a scrivere codice poniamoci qualche domanda:
 Che ci sia un array da qualche parte?
 Se dobbiamo confrontare qualcosa che "cosa" ci serve?
